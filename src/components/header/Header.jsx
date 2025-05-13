70bb3878578de765de4f05527595b7e0b84eca45
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2025-05-13 16:57:43 +0500

    ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -49,13 +49,13 @@ const Header =() =>{
                             <i className='uil uil-briefcase-alt nav__icon'></i>Services
                         </a>
                     </li>
-                    <li className='nav__item'>
+                    {/*<li className='nav__item'>
                         <a href='#portfolio' 
                         onClick={()=>{setActiveNav('#portfolio')}} 
                         className={activeNav === '#portfolio' ? "nav__link active-link" : "nav__link"}>
                             <i className='uil uil-scenery nav__icon'></i>Portfolio
                         </a>
-                    </li>
+                    </li>*/} 
                     <li className='nav__item'>
                         <a href='#contact' 
                         onClick={()=>{setActiveNav('#contact')}} 
